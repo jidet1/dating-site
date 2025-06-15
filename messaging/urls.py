b1cb74117cf598e2c4e793c0fb9a4f644656059e
joshuaagbeluyi20@gmail.com <joshuaagbeluyi20@gmail.com>
2025-06-15 23:59:25 +0100

    real time messsaging

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import ConversationDetailView
 app_name = 'messaging'
 
 urlpatterns = [
     path('conversations/', views.ConversationListView.as_view(), name='conversation_list'),
-    path('conversation/<int:match_id>/', views.ConversationDetailView.as_view(), name='conversation_detail'),
+    path('conversation/<int:pk>/', ConversationDetailView.as_view(), name='conversation_detail'),
     path('send-message/', views.SendMessageView.as_view(), name='send_message'),
 ]
